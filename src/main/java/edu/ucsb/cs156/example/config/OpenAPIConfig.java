d961dffd59d5e2cfb926baa08d607f332de4ed57
Phillip Conrad <pconrad@cs.ucsb.edu>
2025-03-30 17:54:37 -0700

    pc - update home page and add tests for logged out, logged in, and logged in as admin

--- src/main/java/edu/ucsb/cs156/example/config/OpenAPIConfig.java
+++ src/main/java/edu/ucsb/cs156/example/config/OpenAPIConfig.java
@@ -14,6 +14,8 @@ import io.swagger.v3.oas.annotations.servers.Server;
   description = """
     <p><a href='/'>Home Page</a></p>
     <p><a href='/h2-console'>H2 Console (only on localhost)</a></p>
+    <p><a href='/login'>login</a></p>
+    <p><a href='/logout'>logout</a></p>
     """     
     ),
   servers = @Server(url = "/")
--- src/main/java/edu/ucsb/cs156/example/controllers/HomepageController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/HomepageController.java
@@ -1,21 +1,51 @@
 package edu.ucsb.cs156.example.controllers;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.ucsb.cs156.example.services.CurrentUserService;
+
 @RestController
 public class HomepageController {
-    @GetMapping("/")
-    public String index(){
-        return """
-                <p>This is the homepage for team01 which is simply a backend with no frontend.</p>
-                <p><ul>
-                  <li><a href="/oauth2/authorization/google">To sign in, click here</a></li>
-                  <li><a href="/logout">To sign out, click here</a></li>
-                  <li><a href="/swagger-ui/index.html">Click here to go to Swagger</a></li>
-                </ul></p>
-                """;
+
+  @Autowired
+  CurrentUserService currentUserService;
+
+  @GetMapping("/")
+  public String index() {
+    String HomePageHTMLTemplate = """
+        <p>This is the homepage for team01 which is simply a backend with no frontend.</p>
+        <p>
+          <ul>
+            %s
+            %s
+            %s
+            <li><a href="/swagger-ui/index.html">Swagger API Links</a></li>
+            <li><a href="/h2-console">H2 console (only on localhost)</a></li>
+          </ul>
+        </p>
+        """;
+    return String.format(HomePageHTMLTemplate, getLoggedInAs(), getLoginLogoutLink(), getRoles());
+  }
+
+  private String getLoginLogoutLink() {
+
+    return currentUserService.isLoggedIn() ?
+      """ 
+      <li><a href="/logout">Logout</a></li>""" :
+      """
+      <li><a href="/oauth2/authorization/google">Login</a></li>""" ;
     }
+
+  private String getLoggedInAs() {
+    return currentUserService.isLoggedIn()
+        ? String.format("<li>Currently logged in as %s</li>", currentUserService.getUser().getEmail())
+        : "<li>Not logged in</li>";
+  }
+
+  private String getRoles() {
+    return String.format("<li>Roles: %s</li>", currentUserService.getRolesSorted());
+  }
 
 }
